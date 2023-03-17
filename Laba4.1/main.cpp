bd8fbd181947c341e0754bba1777086e9550115c
kseniaDubova <ksushadubova@icloud.com>
2023-03-17 15:53:36 +0400

    Initial Commit

---
+++ Laba4.1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Laba4.1
+//
+//  Created by Ксения Дубова on 17.03.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
